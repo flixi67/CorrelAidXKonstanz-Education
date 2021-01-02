5423272f499e2e4ca79e8eb50ceb91494513ec73
LianeStrauch <liane_strauch@yahoo.com>
2021-01-02 16:25:51 +0100

    Update script.R

--- What-is-R/script.R
+++ What-is-R/script.R
@@ -4,4 +4,10 @@
 # What is R?            #
 #########################
 
+## aktuellen Pfad abfragen
+getwd()
+
+## Pfad ändern
+setwd() #in die Klammern gehört der Pfad, der gesetzt werden soll in Anführungszeichen
+
 ## Laden von packages
