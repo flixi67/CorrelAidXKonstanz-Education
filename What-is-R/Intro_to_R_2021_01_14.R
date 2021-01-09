f5b81604616bb23583e62938098242ea5ff085aa
flixi67 <felix.kube@uni-konstanz.de>
2021-01-09 16:10:36 +0100

    exercises for inbetween the sessions

---
+++ What-is-R/Intro_to_R_2021_01_14.R
@@ -0,0 +1,36 @@
+## ---------------------------
+##
+## Script name: CorrelAid-X_Konstanz_Education
+##
+## Purpose of script: Script to show basic usage of R
+##
+## Author: Felix Kube
+##
+## Date Created: 2021-01-09
+##
+## Email: konstanz@correlaid.de
+##
+## ---------------------------
+##
+## Notes:
+## - Einführung in Verzeichnisse, beim Daten runterladen und in R einspielen automatisch weitere Ordner erstellen
+## - diese dann wieder mit dir() zeigen/ oder in Windows
+##
+## ---------------------------
+
+
+
+# R-Projekte, working directory und Pfade ----------------------------------
+
+getwd() # zeigt unser aktuelles Verzeichnis (bei R-Projekten automatisch Verzeichnis, in dem das R-Projekt liegt)
+        # was hier zurück kommt, nennen wir einen absoluten Pfad (fängt mit C:/ an)
+        # Absolute Pfade sehen auf jedem Computer anders aus!!!
+dir()   # zeigt Dateien im Verzeichnis
+dir.create(paste0(getwd(), "/data")) # Unterordner "data" erstellen
+write.csv(my_number_list, file = "data/numbers.csv") # das Objekt "integers" als Datei exportieren
+
+dir("/data")  # die Funktion dir() nimmt einen relativen Pfad, der auf dem aktuellen Arbeitsverzeichnis aufbaut
+              # dadurch ist es unabhängig vom Computer, auf dem man es ausführt
+              # um reproduzierbare Skripte zu produzieren, sollte man auf absolute Pfade verzichten
+
+# 
---
+++ What-is-R/Intro_to_R_2021_exercises-with-solutions.R
@@ -0,0 +1,44 @@
+## ---------------------------
+##
+## Script name: CorrelAid-X_Konstanz_Education Übungsaufgaben
+##
+## Purpose of script: Training exercises for basic R functionality
+##
+## Author: Felix Kube
+##
+## Date Created: 2021-01-09
+##
+## Email: konstanz@correlaid.de
+##
+## ---------------------------
+##
+## Notes:
+##
+## ---------------------------
+
+# Übung 1 ----------------------------------------------------------------
+# Erstelle einen Charaktervektor mit dem Namen "obst" und den Elementen "banane", "apfel", "traube" und "kiwi"
+obst <- c("banane", "apfel", "traube", "kiwi")
+
+# Erstelle einen numerischen Vektor "bewertung" mit den Zahlen 1 bis 10
+bewertung <- 1:10
+
+# Ordne die Zahlen im Vektor "bewertung" zufällig neu an und speichere die ersten 4
+bewertung <- sample(bewertung, size = 4)
+
+# Füge die beiden Vektoren "obst" und "bewertung" in einer Liste zusammen
+liste <- list(obst, bewertung)
+
+# Gib die Elemente der Liste zurück. Welches Obst wurde bei dir am besten bewertet?
+liste
+
+# Bonus: Schreibe eine Funktion, die nur das Obst mit der besten Bewertung zurück gibt.
+#        Mit der Funktion which.max() kannst du die Position der größten Zahl eines Vektors bestimmen
+#        Die Elemente in Listen abzurufen ist etwas tricky. Probiere ruhig etwas rum.
+beste_bewertung <- function() {
+  index <- which.max(liste[[2]])
+  return(liste[[1]][index])
+}
+beste_bewertung()
+
+# Übung 2 -------------------------------------------------------------------
