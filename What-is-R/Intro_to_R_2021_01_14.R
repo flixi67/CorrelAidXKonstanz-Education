ae1ceee12b56b3bfdb1ed86476540fe8cf69a60d
Philipp Bosch <pmbosch94@gmail.com>
2021-01-14 15:52:26 +0100

    Fix typo on line 118
    replace "df" with "clean_vote_konstanz_2016"

--- What-is-R/Intro_to_R_2021_01_14.R
+++ What-is-R/Intro_to_R_2021_01_14.R
@@ -114,8 +114,8 @@ filter(clean_vote_konstanz_2016, Wahlberechtigte_insgesamt > 800)
 filter(clean_vote_konstanz_2016, Stadtteil == 'Altstadt' | Stadtteil == 'Petershausen-West' |
          Stadtteil == 'Staad')
 
-# Elegantere Lösung
-filter(df, Stadtteil %in% c('Altstadt', 'Petershausen-West', 'Staad'))
+# Elegantere Lösung (!!! Error in inital script !!!)
+filter(clean_vote_konstanz_2016, Stadtteil %in% c('Altstadt', 'Petershausen-West', 'Staad'))
 
 
 # der Pipe Operator %>% ---------------------------------------------------
